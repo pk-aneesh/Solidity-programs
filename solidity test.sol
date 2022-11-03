8f029a52c22c45d54a1e075588a0b42066c768c7
pk-aneesh <117084635+pk-aneesh@users.noreply.github.com>
2022-11-03 13:55:07 +0530

    Create solidity test.sol

---
+++ solidity test.sol
@@ -0,0 +1,14 @@
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.5.0;
+
+contract SolidityTest {
+   constructor() public{
+   }
+   function getResult() public view returns(uint){
+      uint a = 1;
+      uint b = 2;
+      uint result = a + b;
+      return result;
+   }
+}
